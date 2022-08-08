b7afa543725b13595f0b439765a4a248bf7ab287
Andrea Abaunza <andrea.mabalu@gmail.com>
2022-08-08 14:34:24 -0400

    EmployeePage

---
+++ vue/src/views/EmployeeHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <heading />
+    <div>This is an employee page</div>
+  </div>
+</template>
+
+<script>
+import Heading from "@/components/Heading.vue";
+
+export default {
+  name: "employee-home",
+  components: {
+    Heading,
+  },
+};
+</script>
+
+<style>
+</style>
