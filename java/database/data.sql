d23b3b6e22768cecb383394326258eaa54e95e40
Kevin Marcum <marcum.k@gmail.com>
2022-08-08 10:33:06 -0400

    Added initial sql scripts

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,6 +1,14 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role, first_name, last_name, email_address, phone_number)
+    VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'User', 'User', 'user@techelevator.com', '555-5555');
+INSERT INTO users (username,password_hash,role, first_name, last_name, email_address, phone_number)
+    VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'Admin', 'Admin', 'admin@techelevator.com', '555-5555');
+
+INSERT INTO customers(customer_id, user_id) VALUES (1, 1);
+
+INSERT INTO vehicles(vehicle_id, customer_id, make, model, year, color) VALUES (1, 1, 'Honda', 'Accord', '2012', 'blue');
+
+INSERT INTO requests (request_id, customer_id, vehicle_id, description) VALUES (1, 1, 1, 'my car is broke');
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,71 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS repair_items, work_orders, requests, vehicles, customers, employees, users;
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	email_address varchar(50) NOT NULL,
+	phone_number varchar(12) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE employees (
+    employee_id SERIAL,
+    user_id int NOT NULL,
+    CONSTRAINT PK_employee_employee_id PRIMARY KEY (employee_id),
+    CONSTRAINT FK_employee_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE customers (
+    customer_id SERIAL,
+    user_id int NOT NULL,
+    CONSTRAINT PK_customer_customer_id PRIMARY KEY (customer_id),
+    CONSTRAINT FK_customer_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE vehicles (
+    vehicle_id SERIAL,
+    customer_id int NOT NULL,
+    make varchar(100) NOT NULL,
+    model varchar(100) NOT NULL,
+    year int NOT NULL,
+    color varchar(50) NOT NULL,
+    CONSTRAINT PK_vehicle_vehicle_id PRIMARY KEY (vehicle_id),
+    CONSTRAINT FK_vehicle_customer_id FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE requests (
+    request_id SERIAL,
+    customer_id int NOT NULL,
+    vehicle_id int NOT NULL,
+    description varchar(150) NOT NULL,
+    CONSTRAINT PK_request_request_id PRIMARY KEY (request_id),
+    CONSTRAINT FK_request_customer_id FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    CONSTRAINT FK_request_vehicle_id FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id)
+);
+
+CREATE TABLE work_orders (
+    work_order_id SERIAL,
+    employee_id int NOT NULL,
+    all_completed boolean NOT NULL,
+    time_completed timestamp NOT NULL,
+    CONSTRAINT PK_work_order_work_order_id PRIMARY KEY (work_order_id),
+    CONSTRAINT FK_work_order_employee_id FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
+
+CREATE TABLE repair_items (
+    repair_item_id SERIAL,
+    work_order_id int NOT NULL,
+    parts_cost decimal(13, 2) NOT NULL,
+    labor_cost decimal(13, 2) NOT NULL,
+    duration int NOT NULL,
+    CONSTRAINT PK_repair_item_repair_item_id PRIMARY KEY (repair_item_id),
+    CONSTRAINT FK_repair_item_work_order_id FOREIGN KEY (work_order_id) REFERENCES work_orders(work_order_id)
 );
 
 COMMIT TRANSACTION;
