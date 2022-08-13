b0cd6696c83057aa700bde26ac621e5b6d269410
Kevin Marcum <marcum.k@gmail.com>
2022-08-13 11:30:40 -0400

    Add vehicle fix

--- vue/src/services/APIService.js
+++ vue/src/services/APIService.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://car-data.p.rapidapi.com/cars/years',
-//     headers: {
-//         'X-RapidAPI-Key': '3ce156a0cbmsh3cdebac2f07584ap1f99dejsn462087aef6d1',
-//         'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
-//     }
-// };
 
 const http = axios.create({
-    method: 'GET',
     baseURL: 'https://car-data.p.rapidapi.com/cars',
     headers: {
         'X-RapidAPI-Key': '3ce156a0cbmsh3cdebac2f07584ap1f99dejsn462087aef6d1',
