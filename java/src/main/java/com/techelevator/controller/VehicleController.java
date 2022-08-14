c880b1d63cb3ff296ab3cc0b0a993f5eb75f0672
Kevin Marcum <marcum.k@gmail.com>
2022-08-13 22:33:18 -0400

    Moved api call to back-end

--- java/src/main/java/com/techelevator/controller/VehicleController.java
+++ java/src/main/java/com/techelevator/controller/VehicleController.java
@@ -3,6 +3,8 @@ package com.techelevator.controller;
 import com.techelevator.dao.VehicleDao;
 import com.techelevator.model.Vehicle;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -20,4 +22,41 @@ public class VehicleController {
 
     @RequestMapping(path = "/vehicle/{customerId}", method = RequestMethod.GET)
     public List<Vehicle> getVehicleByCustomerId(@PathVariable int customerId) {return vehicleDao.getVehicleByCustomerId(customerId);}
+
+    @RequestMapping(path = "/vehicle/years", method = RequestMethod.GET)
+    public String getVehicleYears() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://car-data.p.rapidapi.com/cars/years"))
+                .header("X-RapidAPI-Key", "3ce156a0cbmsh3cdebac2f07584ap1f99dejsn462087aef6d1")
+                .header("X-RapidAPI-Host", "car-data.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    @RequestMapping(path = "/vehicle/makes", method = RequestMethod.GET)
+    public String getVehicleMakes() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://car-data.p.rapidapi.com/cars/makes"))
+                .header("X-RapidAPI-Key", "3ce156a0cbmsh3cdebac2f07584ap1f99dejsn462087aef6d1")
+                .header("X-RapidAPI-Host", "car-data.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    @RequestMapping(path = "/vehicle/models/{year}/{make}", method = RequestMethod.GET)
+    public String getVehicleModels(@PathVariable String year, @PathVariable String make) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(UriComponentsBuilder.fromUriString("https://car-data.p.rapidapi.com/cars").queryParam("year", year).queryParam("make", make).build().toUri())
+                .header("X-RapidAPI-Key", "3ce156a0cbmsh3cdebac2f07584ap1f99dejsn462087aef6d1")
+                .header("X-RapidAPI-Host", "car-data.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
 }
--- vue/src/services/APIService.js
+++ vue/src/services/APIService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 
-const http = axios.create({
-    baseURL: 'https://car-data.p.rapidapi.com/cars',
-    headers: {
-        'X-RapidAPI-Key': '3ce156a0cbmsh3cdebac2f07584ap1f99dejsn462087aef6d1',
-        'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
-    }
-});
+// const http = axios.create({
+//     baseURL: 'https://car-data.p.rapidapi.com/cars',
+//     headers: {
+//         'X-RapidAPI-Key': '3ce156a0cbmsh3cdebac2f07584ap1f99dejsn462087aef6d1',
+//         'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
+//     }
+// });
 
 export default {
 
@@ -19,15 +19,19 @@ export default {
     // },
 
     getVehicleYears() {
-        return http.get('/years');
+        return axios.get('/vehicle/years');
     },
 
     getVehicleMakes() {
-        return http.get('/makes');
+        return axios.get('/vehicle/makes');
     },
 
     getVehicleModels(year, make) {
-        return http.get(`?year=${year}&make=${make}`);
+        return axios.get(`/vehicle/models/${year}/${make}`);
     }
+
+    // getVehicleModels(year, make) {
+    //     return http.get(`?year=${year}&make=${make}`);
+    // }
 
 }
